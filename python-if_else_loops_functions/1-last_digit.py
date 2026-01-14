952ee5ece7faa6c6f2322c1a361fd7c19d5af133
aragoza <charlet.elliot@gmail.com>
2026-01-14 20:41:32 +0100

    forgot to keep the sign on the last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+
 if number < 0:
-    positive = -number
-else:
-    positive = number
+    positive = -(-number %10)
+elif number >=0:
+    positive = number %10
     
-if positive % 10 > 5:
-    print(f"Last digit of {number} is {positive % 10} and is greater than 5")
-elif positive % 10 == 0:
-    print(f"Last digit of {number} is {positive % 10} and is 0")
-else:
-    print(f"Last digit of {number} is {positive % 10} and is less than 6 and not 0")
+if positive > 5:
+    print(f"Last digit of {number} is {positive} and is greater than 5")
+elif positive == 0:
+    print(f"Last digit of {number} is {positive} and is 0")
+elif positive < 6 and positive % 10 != 0:
+    print(f"Last digit of {number} is {positive} and is less than 6 and not 0")
