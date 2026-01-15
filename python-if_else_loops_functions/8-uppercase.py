0e10cb71f8e73fe2474dbb6714800e2a36ad0752
aragoza <charlet.elliot@gmail.com>
2026-01-15 08:45:01 +0100

    append a space

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
     for c in str:
         if ord(c) >= ord('a') and ord(c) <= ord('z'):
             c = chr(ord(c) - 32)
-        print("{:c}".format(ord(c)),end="")
+        print("{:c}".format(ord(c)), end="")
     print()
