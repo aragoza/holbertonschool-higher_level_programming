7b849bd91cdf780f388b514f10184b635a6419dc
aragoza <charlet.elliot@gmail.com>
2026-01-16 14:51:52 +0100

    apend a __name__ to avoid the execute of the programm on the import

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-def main():
-    from add_0 import add
+from add_0 import add
+
+
+if __name__ == "__main__":
     a = 1
     b = 2
     print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
