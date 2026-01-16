a530df4df2b88eebeb09345bdb0dfcdf14ae16af
aragoza <charlet.elliot@gmail.com>
2026-01-16 21:18:48 +0100

    forgot a semicolon :

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
         print("1 argument:")
         print("1: {:s}".format(sys.argv[1]))
     else:
-        print("{:d} arguments".format(len(sys.argv)-1))
+        print("{:d} arguments:".format(len(sys.argv)-1))
         for i in range(1, len(sys.argv)):
             print("{:d}: {:s}".format(i, sys.argv[i]))
