bc5e0f27dd0da63ed4b8ec645b6472fdd35202a3
aragoza <charlet.elliot@gmail.com>
2026-01-20 11:22:31 +0100

    change the code to work with the test on a tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if tuple_a[0] == None:
-        tuple_a[0] = 0
-    if tuple_a[1] == None:
-        tuple_a[1] = 0
-    if tuple_b[0] == None:
-        tuple_b[0] = 0
-    if tuple_b[1] == None:
-        tuple_b[1] = 0
+    if len(tuple_a) == 0:
+        if len(tuple_b) == 0:
+            return (0 + 0, 0 + 0)
+        elif len(tuple_b) == 1:
+            return (0 + tuple_b[0], 0 + 0)
+        else:
+            return (0 + tuple_b[0], 0 + tuple_b[1])
 
-    tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return tuple_c
+    elif len(tuple_a) == 1:
+        if len(tuple_b) == 0:
+            return (tuple_a[0] + 0, 0 + 0)
+        elif len(tuple_b) == 1:
+            return (tuple_a[0] + tuple_b[0], 0 + 0)
+        else:
+            return (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
+
+    else:
+        if len(tuple_b) == 0:
+            return (tuple_a[0] + 0, tuple_a[1] + 0)
+        elif len(tuple_b) == 1:
+            return (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
+        else:
+            return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
