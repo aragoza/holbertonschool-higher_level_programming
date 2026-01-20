deda7d68d762525f292479d89201ebead56fe644
aragoza <charlet.elliot@gmail.com>
2026-01-20 14:01:59 +0100

    change the style and the method to check for negative and out of range

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx == None:
+    if idx not in range(len(my_list)):
         return None
     for i in range(len(my_list)):
         if i == idx:
             return my_list[i]
-    return None
