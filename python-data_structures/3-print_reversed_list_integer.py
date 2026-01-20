6194ede6f44e4f19f131ee3370d7ee6db1a9eb8e
aragoza <charlet.elliot@gmail.com>
2026-01-20 14:24:43 +0100

    chagne style and method

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -4,7 +4,3 @@
 def print_reversed_list_integer(my_list=[]):
     for i in range(len(my_list)):
         print("{:d}".format(my_list[-i-1]))
-
-
-if __name__ == "__main__":
-    print_reversed_list_integer(my_list)
