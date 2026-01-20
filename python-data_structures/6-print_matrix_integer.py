22cb5b3ad8544804c5b3e51fb4d16a547f40acc1
aragoza <charlet.elliot@gmail.com>
2026-01-20 15:02:24 +0100

    change the method to match the result attempted

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
-def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        print("{}".format(matrix[i]))
+def print_matrix_integer(matrix=[[]]): 
+    if matrix == [[]]:
+        print()
+    else:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])-1):
+                print("{:d}".format(matrix[i][j]), end=" ")
+            print("{:d}".format(matrix[i][len(matrix[i]) - 1]))
