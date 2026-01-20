aa6fd8be7b3e260668ea186c93f811eddae24ac4
aragoza <charlet.elliot@gmail.com>
2026-01-20 14:29:17 +0100

    chagne the method to avoid the change on the original list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,15 +2,10 @@
 
 
 def new_in_list(my_list, idx, element):
-    if idx == None:
+    new_list = []
+    if idx not in range(len(my_list)):
         return my_list
-    elif idx <= len(my_list) and idx >= 0:
-        new_list = my_list
+    elif idx in range(len(my_list)):
+        new_list = my_list.copy()
         new_list[idx] = element
         return new_list
-    else:
-        return my_list
-
-
-if __name__ == "__main__":
-    print("{}".format(new_in_list(my_list, idx, element)))
