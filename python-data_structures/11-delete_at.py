c1ca9ff9778e3761cb3383f7d6db67e3c258ef44
aragoza <charlet.elliot@gmail.com>
2026-01-21 11:00:46 +0100

    change the method, add all the element of the origin list to a new list except the element at idx

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    if idx in range(len(my_list)):
-            my_list[idx] = []
-    return my_list
+    new_list = []
+    for i in range(len(my_list)):
+        if i == idx:
+            continue
+        new_list.append(my_list[i])
+    return new_list
