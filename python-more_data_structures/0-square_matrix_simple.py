99716bdc8a9df3ca82dd1ea9625c42a619edd9d1
aragoza <charlet.elliot@gmail.com>
2026-01-21 11:52:07 +0100

    append a white space around operator l6

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for i in range(len(matrix)):
-        sub_matrix=[]
+        sub_matrix = []
         for j in range(len(matrix[i])):
             x = matrix[i][j] * matrix[i][j]
             sub_matrix.append(x)
