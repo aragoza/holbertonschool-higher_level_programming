559b26d2a47302039968eb07de285ef739bd9dd3
aragoza <charlet.elliot@gmail.com>
2026-01-21 14:00:56 +0100

    change the method to detect keys

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -2,8 +2,6 @@
 
 def number_keys(a_dictionary):
     count = 0
-    for i in a_dictionary:
-        if a_dictionary.get(a_dictionary[i], False) == False:
-            count += 1
-
+    for keys, value in a_dictionary.items():
+        count += 1
     return count
