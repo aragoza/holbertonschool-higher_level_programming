bd35cd9c2c7f4b7ea3c162d8fb09470db9b0bd3a
aragoza <charlet.elliot@gmail.com>
2026-01-24 15:59:05 +0100

    return a new dict with all value mul by 2

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary.copy()
+    for i in a_dictionary:
+        new_dict[i] = 2*a_dictionary[i]
+
+    return new_dict
