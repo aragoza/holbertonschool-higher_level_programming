cecb0dfb497eb21b26580b5aaf67cb421dedce95
aragoza <charlet.elliot@gmail.com>
2026-01-24 16:32:41 +0100

    return the best value in the dict

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    
-    if a_dictionary == None:
+
+    if a_dictionary is None or a_dictionary == {}:
         return None
 
-    b = "yes"    
+    b = "yes"
 
     for key in a_dictionary:
         if b == "yes":
