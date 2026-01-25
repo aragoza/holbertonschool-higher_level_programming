a0af1f64d5066d2eb6b2b611d79d8ca830fd506f
aragoza <charlet.elliot@gmail.com>
2026-01-25 13:31:55 +0100

    change the type affectation and add a return

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    result = ((int) or (None))
+    result = None
     try:
         result = a / b
     except (TypeError, ValueError, ZeroDivisionError):
         result = None
     finally:
         print("Inside result: {}".format(result))
+
+    return result
