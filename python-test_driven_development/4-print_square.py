64d17d79c262a99f5f6f93a3e1c4bda1c9fb8876
aragoza <charlet.elliot@gmail.com>
2026-01-25 21:07:36 +0100

    function that prints a square with the character #

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Module that defines a function to print a square using the character '#'.
+
+The square has a size equal to the given integer argument. Each row and
+column contains exactly `size` number of '#' characters.
+"""
+
+
+def print_square(size):
+    """
+    Prints a square with the character '#'.
+
+    The size of the square is determined by the value of `size`. Each row
+    and each column of the square is composed of `size` number of '#'
+    characters.
+
+    Args:
+        size (int): the size length of the square
+
+    Raises:
+        TypeError: if size is not an integer
+        ValueError: if size is less than 0
+    """
+    if type(size) is float and size < 0:
+        raise TypeError("div must be a number")
+
+    if type(size) is not int:
+        raise TypeError("div must be a number")
+    
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
+
+print_square(4)
+print("")
+print_square(10)
+print("")
+print_square(0)
+print("")
+print_square(1)
+print("")
+try:
+    print_square(-1)
+except Exception as e:
+    print(e)
+print("")
