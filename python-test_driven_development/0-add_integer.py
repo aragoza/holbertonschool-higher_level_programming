4b32e2895522b69577ec0180e9aabfc32458a0fd
aragoza <charlet.elliot@gmail.com>
2026-01-25 18:45:57 +0100

    change the calcul method on float again

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -23,9 +23,9 @@ def add_integer(a, b=98):
     Raises:
         TypeError: if a or b is not an integer or float
     """
-    if type(a) not in (int, float):
+    if type(a) not in (int, float) or a == None:
         raise TypeError("a must be an integer")
-    if type(b) not in (int, float):
+    if type(b) not in (int, float) or b == None:
         raise TypeError("b must be an integer")
 
     if type(a) is float:
