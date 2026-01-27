095050a1c5b521bd408b2bae7803cf828a9a1488
aragoza <charlet.elliot@gmail.com>
2026-01-27 15:01:35 +0100

    remove the test

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -21,18 +21,3 @@ class Square:
         if not isinstance(self.size, int):
             raise TypeError("size must be an integer")
         return self.size * self.size
-    
-
-
-
-my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-my_square.size = 3
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-try:
-    my_square.size = "5 feet"
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-except Exception as e:
-    print(e)
