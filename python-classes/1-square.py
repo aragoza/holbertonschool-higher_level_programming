bbceaeba552a50822ae1ea066e842677a9950549
aragoza <charlet.elliot@gmail.com>
2026-01-27 10:46:15 +0100

    do the first instancies of a class

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+def of a class
+"""
+
 
 class Square:
+    """
+    Docstring for Square (size private instance)
+    """
 
     def __init__(self, size):
         self.__size = size
