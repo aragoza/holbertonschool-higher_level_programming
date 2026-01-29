aad6a4bcf779996fb93b8f0c4baad83d2b35c6ad
aragoza <charlet.elliot@gmail.com>
2026-01-29 13:39:06 +0100

    redo all the task based on the redid previous task

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -7,28 +7,32 @@ Class Square will create a square at the end and verify everything
 
 class Square:
     """
-    Class Square
+    Class Square define a new type where you can set the size of your square
     """
-    def __init__(self, size):
-        if not isinstance(size, int):
+# init
+    def __init__(self, size=0):
+        self.__size = size
+
+# get
+    @property
+    def size(self):
+        return self.__size
+
+# set
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.size = size
+        self.__size = value
 
-    def size(self, value: int):
-        self.size = value
-
-    def size(self: int) -> int:
-        return self.size
-    
-    def area(self: int) -> int:
-        if not isinstance(self.size, int):
-            raise TypeError("size must be an integer")
-        return self.size * self.size
+    def area(self):
+        return self.__size ** 2
 
     def my_print(self):
-        if self.size == 0:
+        if self.__size == 0:
             print()
-        else:
-            print(("#" * self.size + "\n") * self.size, end="")
+            pass
+        for i in range(self.__size):
+            print('#' * self.__size)
