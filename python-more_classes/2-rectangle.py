7d1276f5672d4dea2254dd0d83a901884be54bf9
aragoza <charlet.elliot@gmail.com>
2026-01-29 22:15:21 +0100

    do the area and the permiter of the rectangle

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+"""
+New class named Rectangle : a rectangle have 4 angle to side parallel
+"""
+
+
+class Rectangle:
+    """
+    class Rectangle set and get the width and height
+    """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+# get
+    @property
+    def width(self):
+        return self.__width
+# set
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+# get
+    @property
+    def height(self):
+        return self.__height
+# set
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+# area
+    def area(self):
+        return self.__width * self.__height
+
+#perimeter
+    def perimeter(self):
+        return ((2 * self.__height) + (2 * self.__width))
