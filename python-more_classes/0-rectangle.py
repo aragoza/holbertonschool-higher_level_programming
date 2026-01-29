6b0d76cec327c249e3991bd02a2693323a611ede
aragoza <charlet.elliot@gmail.com>
2026-01-29 13:41:33 +0100

    empty class rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+New class named Rectangle : a rectangle have 4 angle to side parallel
+"""
+
+
+class Rectangle:
+    """
+    class Rectangle is empty
+    """
+    pass
