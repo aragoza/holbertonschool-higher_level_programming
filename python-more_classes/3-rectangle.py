bae203a5449525e92cc21795330a72a395cedcbe
aragoza <charlet.elliot@gmail.com>
2026-01-30 09:58:28 +0100

    do the str for the print and str function

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -56,18 +56,3 @@ class Rectangle:
         a = self.__height
         b = self.__width
         return f"{("#" * b + "\n") * (a - 1) + "#" * b}"
-
-
-
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-print(str(my_rectangle))
-print(repr(my_rectangle))
-
-print("--")
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle)
-print(repr(my_rectangle))
