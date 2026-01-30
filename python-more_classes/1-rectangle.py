31385affb012b1e4477e7a7ab7060546f0723516
aragoza <charlet.elliot@gmail.com>
2026-01-30 17:11:05 +0100

    the private attribute is did in the getter and the setter so i remove the private in the constructor

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
     class Rectangle set and get the width and height
     """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
 # get
     @property
@@ -40,3 +40,10 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+my_rectangle = Rectangle(2, 4)
+print(my_rectangle.__dict__)
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle.__dict__)
