0d4d6ec9f5d9ed91043d350ccd19891c383a50d4
aragoza <charlet.elliot@gmail.com>
2026-02-02 16:02:35 +0100

    add the premices of the method area

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""BaseGeometry
+Empty class for a base of geometry
+"""
+
+
+class BaseGeometry():
+    """
+    a base of geometry
+    """
+    
+    def area(self):
+        raise Exception("area() is not implemented")
