dae29552c372468581891836ba967ac73243ba65
aragoza <charlet.elliot@gmail.com>
2026-02-03 09:56:02 +0100

    init the attribute size

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Class Square that init size base on the super class Rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class Square
+    """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
