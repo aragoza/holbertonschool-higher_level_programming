df2e5d8d7d321734663dc647b8f6e23291de76f2
aragoza <charlet.elliot@gmail.com>
2026-02-03 14:01:23 +0100

    circle and rectangle that inherits from shape class

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+"""
+Docstring for holbertonschool-higher_level_programming.python-abc.task_00_abc
+"""
+
+from math import pi
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    """
+    Docstring for Animal
+    """
+
+    @abstractmethod
+    def area(self):
+        ...
+
+    @abstractmethod
+    def perimeter(self):
+        ...
+
+class Circle(Shape):
+    """
+    Docstring for Dog
+    """
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return pi * self.radius * self.radius
+
+    def perimeter(self):
+        return 2 * pi * self.radius
+
+class Rectangle(Shape):
+    """
+    Docstring for Cat
+    """
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+    
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+def shape_info(self):
+    print("{}: {}".format("Area", self.area()))
+    print("{}: {}".format("perimeter", self.perimeter()))
