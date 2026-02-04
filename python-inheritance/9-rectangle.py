f94d62172cc8f68691ff36ee6dddf06b6acf6cc7
aragoza <charlet.elliot@gmail.com>
2026-02-04 13:06:59 +0100

    append docstring for every method

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -7,20 +7,38 @@ and the method area and str for human representation
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """
     Class Rectangle
     """
 
     def __init__(self, width, height):
+        """
+        Docstring for __init__
+
+        :param self: Description
+        :param width: Description
+        :param height: Description
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def __str__(self):
-        return "[{}] {}/{}".format(self.__class__.__name__, self.__width, self.__height)
-    
+        """
+        Docstring for __str__
+
+        :param self: Description
+        """
+        a = self.__class__.__name__
+        return "[{}] {}/{}".format(a, self.__width, self.__height)
+
     def area(self):
+        """
+        Docstring for area
+
+        :param self: Description
+        """
         return self.__width * self.__height
-    
