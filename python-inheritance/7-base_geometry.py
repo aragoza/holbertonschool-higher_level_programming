8b2c1a2406f31f8fd399d26e5105aa2198419762
aragoza <charlet.elliot@gmail.com>
2026-02-04 13:27:42 +0100

    better docstring for the class and test file to run the tests

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -4,15 +4,27 @@ Empty class for a base of geometry
 """
 
 
-class BaseGeometry():
+class BaseGeometry:
     """
     class that verify the type of every attribute passed in parameter
     """
     
     def area(self):
+        """
+        Docstring for area
+        
+        :param self: Description
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """
+        Docstring for integer_validator
+        
+        :param self: Description
+        :param name: Description
+        :param value: Description
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
