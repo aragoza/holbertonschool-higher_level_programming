aadb8655202f979b5c51ee6d4eae7a0ab85286c4
aragoza <charlet.elliot@gmail.com>
2026-02-06 13:41:31 +0100

    redo the whole task

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,46 +1,43 @@
 #!/usr/bin/python3
 
 """
-Docstring for holbertonschool-higher_level_programming.python-abc.task_00_abc
+3 Class 1 base class and 2 subclass
 """
 
-from math import pi
 
 from abc import ABC, abstractmethod
 
 
+from math import pi
+
+
 class Shape(ABC):
     """
-    Docstring for Animal
+    Class Shape
     """
 
     @abstractmethod
     def area(self):
-        ...
+        """
+        Docstring for area
+
+        :param self: Description
+        """
+        pass
 
     @abstractmethod
     def perimeter(self):
-        ...
+        """
+        Docstring for perimeter
 
-
-class Circle(Shape):
-    """
-    Docstring for Dog
-    """
-
-    def __init__(self, radius):
-        self.radius = radius
-
-    def area(self):
-        return pi * self.radius * self.radius
-
-    def perimeter(self):
-        return 2 * pi * self.radius
+        :param self: Description
+        """
+        pass
 
 
 class Rectangle(Shape):
     """
-    Docstring for Cat
+    Class Rectangle
     """
 
     def __init__(self, width, height):
@@ -48,10 +45,45 @@ class Rectangle(Shape):
         self.height = height
 
     def area(self):
+        """
+        Docstring for area
+
+        :param self: Description
+        """
         return self.width * self.height
 
     def perimeter(self):
+        """
+        Docstring for perimeter
+
+        :param self: Description
+        """
         return 2 * (self.width + self.height)
+
+
+class Circle(Shape):
+    """
+    Class Circle
+    """
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        """
+        Docstring for area
+
+        :param self: Description
+        """
+        return pi * (self.radius ** 2)
+
+    def perimeter(self):
+        """
+        Docstring for perimeter
+
+        :param self: Description
+        """
+        return 2 * pi * self.radius
 
 
 def shape_info(self):
@@ -60,5 +92,5 @@ def shape_info(self):
 
     :param self: Description
     """
-    print("{}: {}".format("Area", self.area()))
-    print("{}: {}".format("perimeter", self.perimeter()))
+    print("Area: {}".format(self.area()))
+    print("Perimeter: {}".format(self.perimeter()))
