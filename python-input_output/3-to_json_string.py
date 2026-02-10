837566e1bdd260b62e098e4c984d4e2249c51859
aragoza <charlet.elliot@gmail.com>
2026-02-10 12:56:44 +0100

    return the obj in JSON format

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Docstring
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Docstring for to_json_string
+    
+    :param my_obj: Description
+    """
+    return json.dumps(my_obj)
