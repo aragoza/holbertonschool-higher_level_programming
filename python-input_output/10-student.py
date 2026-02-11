860b410f786e83ca5e114df821f116d68c403ed7
aragoza <charlet.elliot@gmail.com>
2026-02-11 16:02:29 +0100

    only attribute names contained in this list must be retrieved

---
+++ python-input_output/10-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""
+Docstring
+"""
+
+
+class Student:
+    """
+    Docstring for Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Docstring for __init__
+
+        :param self: Description
+        :param first_name: Description
+        :param last_name: Description
+        :param age: Description
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Docstring for to_json
+
+        :param self: Description
+        """
+        dictionnary = dict()
+
+        if attrs is None:
+            for i in reversed(self.__dict__):
+                dictionnary[i] = self.__dict__[i]
+            return dictionnary
+
+        for k in reversed(self.__dict__):
+            if k in attrs:
+                dictionnary[k] = self.__dict__[k]
+
+        return dictionnary
