62648cd37ddb552ffa61cfcbf42418ee49e72851
aragoza <charlet.elliot@gmail.com>
2026-02-11 13:43:02 +0100

    load, add then write json file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys, json
+
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file 
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+try:
+    temp = load_from_json_file("add_item.json")
+except (FileNotFoundError):
+    temp = []
+
+for i in sys.argv[1:]:
+    temp.append(i)
+
+save_to_json_file(temp, "add_item.json")
