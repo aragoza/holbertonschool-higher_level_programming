41b9173ad6397fce7c2904c35622485f55b1273b
aragoza <charlet.elliot@gmail.com>
2026-02-11 09:58:51 +0100

    do a better docstyle

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -12,5 +12,5 @@ def read_file(filename=""):
     :param filename: Description
     """
 
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         print(f.read())
