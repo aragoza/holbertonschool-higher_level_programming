a40fc5540c2a2a97f8a4a4e67c287e2462086c58
aragoza <charlet.elliot@gmail.com>
2026-02-11 14:58:20 +0100

    return a dictionnary of the element in obj

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+dict the object of obj
+"""
+
+
+def class_to_json(obj):
+    """
+    Docstring for class_to_json
+    
+    :param obj: Description
+    """
+    return obj.__dict__
