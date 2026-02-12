6006b536296e5332d1dae92d588e2aaf48ff2c15
aragoza <charlet.elliot@gmail.com>
2026-02-12 14:19:11 +0100

    serialize then deserialize with pickle module

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+"""
+Docstring for task_01_pickle
+"""
+
+import pickle
+
+
+class CustomObject:
+    """
+    Docstring for CustomObject
+    """
+
+    def __init__(self, name, age, is_student):
+        """
+        Docstring for __init__
+
+        :param self: Description
+        :param name: Description
+        :param age: Description
+        :param is_student: Description
+        """
+        self.age = age
+        self.name = name
+        self.is_student = is_student
+
+    def display(self):
+        """
+        Docstring for display
+
+        :param self: Description
+        """
+        a = self.age
+        n = self.name
+        i = self.is_student
+        print("Name: {}\nAge: {}\nIs Student: {}".format(a, n, i))
+
+    def serialize(self, filename):
+        """
+        Docstring for serialize
+
+        :param self: Description
+        :param filename: Description
+        """
+        with open(filename, 'wb') as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        """
+        Docstring for deserialize
+
+        :param cls: Description
+        :param filename: Description
+        """
+        with open(filename, 'rb') as f:
+            return pickle.load(f)
