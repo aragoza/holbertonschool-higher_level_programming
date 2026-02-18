dc17fbd4eb0de7702b25b9037636ce910e605767
aragoza <charlet.elliot@gmail.com>
2026-02-18 20:43:33 +0100

    fetsch data from a json and print it then fetsch data from json then save it

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    try:
+        url = "https://jsonplaceholder.typicode.com/posts"
+        r = requests.get(url)
+        print(r.status_code)
+        temp = r.json()
+        for i in temp:
+            print("{}".format(i['title']))
+
+    except Exception as e:
+        print("{}".format(e))
+
+
+def fetch_and_save_posts():
+    try:
+        url = "https://jsonplaceholder.typicode.com/posts"
+        r = requests.get(url)
+
+        if r.status_code == 200:
+            data = r.json()
+
+            posts_list = []
+            for post in data:
+                posts_list.append({
+                    "id": post["id"],
+                    "title": post["title"],
+                    "body": post["body"]
+                })
+
+            with open("posts.csv", "w", newline="", encoding="utf-8") as file:
+                fieldnames = ["id", "title", "body"]
+                writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+                writer.writeheader()
+                writer.writerows(posts_list)
+    except Exception as e:
+        print("error: {}".format(e))
