550530d1dde25853a74b5d75cccb01c0206de8fb
aragoza <charlet.elliot@gmail.com>
2026-02-19 15:12:04 +0100

    just for now

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,9 +1,47 @@
-#!!/usr/bin/python3
+#!/usr/bin/python3
 
-import http.server
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
 
-module = http.server.BaseHTTPRequestHandler
-
-class underclass(module):
+class Underclass(BaseHTTPRequestHandler):
     def do_GET(self):
-        self
+        if self.path == "/data":
+            self.send_response(200)
+            data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+            }
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+
+            self.wfile.write(json.dumps(data).encode())
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+
+
+        else:
+            self.send_response(404)
+            print("data not found")
+
+        message = "Hello, this is a simple API!"
+        encode_message = message.encode()
+
+        self.send_response(200)
+
+        self.send_header("Content-Type", "text/plain; charset=utf-8")
+        self.send_header("Content-Length", str(len(encode_message)))
+        self.end_headers()
+
+        self.wfile.write(encode_message)
+
+def run(server_class=HTTPServer, handler_class=Underclass):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
