f3247838a305ccdaae0ffb8da2da59c2de09f095
aragoza <charlet.elliot@gmail.com>
2026-02-19 20:00:21 +0100

    change the status to data and append docstring

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,38 +1,63 @@
 #!/usr/bin/python3
-
+"""
+Docstring for task_03_http_server.py
+"""
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 
 class Underclass(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/data":
+        """
+        Docstring for do_Get
+        """
+        if self.path == "/":
+            message = b"Hello this is a simple API!"
+
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.send_header("Content-Length", str(len(message)))
+            self.end_headers()
+
+            self.wfile.write(message)
+        elif self.path == "/data":
             self.send_response(200)
             data = {
                 "name": "John",
                 "age": 30,
                 "city": "New York"
             }
+            data_serialize = json.dumps(data).encode(encoding="utf-8")
+
             self.send_header("Content-Type", "application/json")
+            self.send_header("Content-Length", str(len(data_serialize)))
             self.end_headers()
+            self.wfile.write(data_serialize)
 
-            self.wfile.write(json.dumps(data).encode(encoding="utf-8"))
+        elif self.path == "/info":
+            info = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+            }
+            info_serialize = json.dumps(info).encode(encoding="utf-8")
 
-        elif self.path == "/status":
             self.send_response(200)
-            self.send_header("Content-Type", "text/plain")
+            self.send_header("Content-Type", "application/json")
+            self.send_header("Content-Length", str(len(info_serialize)))
             self.end_headers()
-
-            self.wfile.write(b"OK")
+            self.wfile.write(info_serialize)
 
         else:
-            self.send_response(404)
-            self.send_error(404, "Not Found", "No {}".format(self.path))
+            self.send_error(404, "Endpoint not found", "Wrong or no Endpoint: {}".format(self.path))
 
 
 
 def run(server_class=HTTPServer, handler_class=Underclass):
+    """
+    Docstring for run
+    """
     server_address = ('', 8000)
     httpd = server_class(server_address, handler_class)
+    print("Server running on http://localhost:8000")
     httpd.serve_forever()
 
 if __name__ == "__main__":
