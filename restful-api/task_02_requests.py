ce416d7f0ba0fb914b57cf1a59e9bee9571be197
aragoza <charlet.elliot@gmail.com>
2026-02-19 14:54:52 +0100

    change the try except for a if else and return

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -5,40 +5,38 @@ import csv
 
 
 def fetch_and_print_posts():
-    try:
-        url = "https://jsonplaceholder.typicode.com/posts"
-        r = requests.get(url)
-        print(r.status_code)
+    url = "https://jsonplaceholder.typicode.com/posts"
+    r = requests.get(url)
+    print(r.status_code)
+
+    if r.status_code != 200:
+        return
+    else:
         temp = r.json()
         for i in temp:
             print("{}".format(i['title']))
 
-    except Exception as e:
-        print("Error: {}".format(e))
-
 
 def fetch_and_save_posts():
-    try:
-        url = "https://jsonplaceholder.typicode.com/posts"
-        r = requests.get(url)
+    url = "https://jsonplaceholder.typicode.com/posts"
+    r = requests.get(url)
 
-        if r.status_code == 200:
-            data = r.json()
+    if r.status_code == 200:
+        data = r.json()
 
-            posts_list = []
-            for post in data:
+        posts_list = []
+        for post in data:
                 posts_list.append({
-                    "id": post["id"],
-                    "title": post["title"],
-                    "body": post["body"]
-                })
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"]
+            })
 
-            with open("posts.csv", "w", newline="", encoding="utf-8") as file:
-                fieldnames = ["id", "title", "body"]
-                writer = csv.DictWriter(file, fieldnames=fieldnames)
+        with open("posts.csv", "w", newline="", encoding="utf-8") as file:
+            fieldnames = ["id", "title", "body"]
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
 
-                writer.writeheader()
-                writer.writerows(posts_list)
-
-    except Exception as e:
-        print("Error: {}".format(e))
+            writer.writeheader()
+            writer.writerows(posts_list)
+    else:
+        return
