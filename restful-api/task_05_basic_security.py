0143a6a041d2f29fd35090ac01fac8fd5ee3b1fe
aragoza <charlet.elliot@gmail.com>
2026-02-20 23:58:19 +0100

    do a some api calls on a server checking for credential

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+Docstring for holbertonschool-higher_level_programming.restful-api.task_05_basic_security
+"""
+
 
 from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth
@@ -22,16 +26,36 @@ users = {
 
 @auth.verify_password
 def verify_password(username, password):
+    """
+    Docstring for verify_password
+
+    :param username: Description
+    :param password: Description
+    :return: Description
+    :rtype: Any | None
+    """
     if username in users and check_password_hash(users[username]["password"], password):
         return username
 
 @app.route("/basic-protected")
 @auth.login_required
 def basic_protected():
+    """
+    Docstring for basic_protected
+
+    :return: Description
+    :rtype: tuple[Response, Literal[200]]
+    """
     return jsonify("Hello {} (Basic Auth)".format(auth.current_user())), 200
 
 @app.route("/login", methods=["POST"])
 def login():
+    """
+    Docstring for login
+
+    :return: Description
+    :rtype: tuple[Response, Literal[200]] | Literal[401]
+    """
     data = request.get_json()
     username = data.get("username")
     password = data.get("password")
@@ -45,6 +69,12 @@ def login():
 @app.route("/jwt-protected", methods=["GET"])
 @jwt_required()
 def jwt_protected():
+    """
+    Docstring for jwt_protected
+
+    :return: Description
+    :rtype: tuple[Response, Literal[200]] | Literal[401]
+    """
     if not get_jwt_identity():
         return 401
     return jsonify("JWT Auth: Access Granted"), 200
@@ -52,6 +82,12 @@ def jwt_protected():
 @app.route("/admin-only", methods=["GET"])
 @jwt_required()
 def admin_only():
+    """
+    Docstring for admin_only
+
+    :return: Description
+    :rtype: tuple[Response, Literal[403]] | tuple[Response, Literal[200]]
+    """
     current_user = get_jwt_identity()
     if users[current_user]["role"] != "admin":
         return jsonify({"error": "Admin access required"}), 403
