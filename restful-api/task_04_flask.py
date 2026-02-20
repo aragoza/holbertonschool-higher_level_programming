8baeceadcbef45711aee52001a13b2f0bd696b8a
aragoza <charlet.elliot@gmail.com>
2026-02-20 11:49:08 +0100

    append the data and data/username endpoints and their meethods

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -4,23 +4,39 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
+
+
+@app.route("/status")
+def go_get_status_code():
+    """
+    Docstring for go_get_status_code
+    """
+    return "OK", 200
+
 @app.route("/")
 def home():
     """
     Docstring for home
     """
-    return "Welcome to the Flask API!"
+    return "Welcome to the Flask API!", 200
 
-@app.route("/data")
+@app.route("/data", methods=["GET"])
 def go_get_data():
     """
     Docstring for get_data
-
-    :param username: Description
     """
-    data_username = [user["username"] for user in users]
-    return jsonify(data_username)
-    
+    data_username = list(users.keys())
+    return jsonify(data_username), 200
+
+@app.route("/data/<string:username>", methods=["GET"])
+def go_go_go_data(username):
+    """
+    Docstring for go_go_go_data
+    """
+    return jsonify(users[username])
+
+
 
 if __name__ == "__main__":
     app.run()
