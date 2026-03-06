56f8ff5ee3b6ae2746cbb84a1f8bee7e3419bfcb
elliot <charlet.elliot@gmail.com>
2026-03-06 16:08:18 +0100

    grant the permission to the user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- create user_0d_1 with password user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON localhost.* TO 'user_0d_1'@'localhost';
