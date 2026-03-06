820dee8e83826cfc496a87ed1befa0b8642a6205
elliot <charlet.elliot@gmail.com>
2026-03-06 17:27:50 +0100

    better method to set id as unique

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,5 @@
--- create a table where the id is always unique
+-- create a table unique_id where the id is always unique
 CREATE TABLE IF NOT EXISTS unique_id (
-    id INT DEFAULT 1,
+    id INT UNIQUE DEFAULT 1,
     name VARCHAR(256)
-    UNIQUE (id)
 );
