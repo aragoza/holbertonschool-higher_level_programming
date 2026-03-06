79f1c8e17e46166c7011b088dd6dcd9fad3c09b3
elliot <charlet.elliot@gmail.com>
2026-03-06 17:23:22 +0100

    better method to unique a column

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,6 @@
 -- create a table where the id is always unique
 CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT UNIQUE AND DEFAULT 1,
+    id INT DEFAULT 1,
     name VARCHAR(256)
+    UNIQUE (id)
 );
