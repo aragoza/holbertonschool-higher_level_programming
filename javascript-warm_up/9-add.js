d6f8ccaa1c4683c352b96703d24221b407248441
elliot <charlet.elliot@gmail.com>
2026-03-20 22:38:22 +0100

    fix the add with stream input argument

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-
 function add (a, b) {
   const i = a + b;
   console.log(i.toString());
 }
+const args = process.argv;
+const a = parseInt(args[2]);
+const b = parseInt(args[3]);
+add(a, b);
