c89a8feddbfb84128be9bede0665e7b9bec65b9b
elliot <charlet.elliot@gmail.com>
2026-03-20 22:13:38 +0100

    first use of an array and a while loop to print 3 msg

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const tableau = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+let i = 0;
+while (i < tableau.length) {
+  console.log(tableau[i]);
+  i++;
+}
