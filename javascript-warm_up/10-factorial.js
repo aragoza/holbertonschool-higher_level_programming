24ce497939076a3904ae7c95bded3e1a0fdf4904
elliot <charlet.elliot@gmail.com>
2026-03-20 22:49:29 +0100

    did the factorial with recursion

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+function factorial(a) {
+    if(a === 0) {
+        return 1;
+    } else {
+        return a * factorial(a - 1);
+    }
+}
+
+const args = process.argv;
+if (args[2]) {
+    console.log(factorial(args[2]))
+} else {
+    console.log(factorial(1))
+}
