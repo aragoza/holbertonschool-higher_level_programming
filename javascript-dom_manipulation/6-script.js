31253446134d53ec4e984d58fc518b779b5064af
elliot <charlet.elliot@gmail.com>
2026-03-27 19:01:57 +0100

    fetch name of leia from api to the html data

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const character = document.getElementById('character')
+
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+    .then(response => response.json())
+    .then(data => {
+      character.textContent = data.name;
+    })
+    .catch(error => {
+      console.error(error);
+    });
